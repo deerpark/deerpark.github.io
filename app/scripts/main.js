aa78ab0b2ca3956a98f959bb86c99f51d78c694a
imac-ji9 <ji9@me.com>
2017-04-23 23:54:07 +0900

    design completed

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,14 @@
-console.log('\'Allo \'Allo!');
+var spy = new ScrollSpy('#nav', {
+	nav: '#nav a',
+	className: 'active'
+});
+
+$(function(){
+	$('#nav a').on('click', function(e){
+		e.preventDefault();
+		var hash = this.hash;
+		$('html,body').animate({
+			scrollTop: $(hash).offset().top
+		},300);
+	});
+});
--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,1 @@
-"use strict";console.log("'Allo 'Allo!");
+"use strict";var spy=new ScrollSpy("#nav",{nav:"#nav a",className:"active"});$(function(){$("#nav a").on("click",function(a){a.preventDefault();var n=this.hash;$("html,body").animate({scrollTop:$(n).offset().top},300)})});
